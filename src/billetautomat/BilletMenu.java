e7a82df10f894d662fbba147d8840dc7e9b28178
Christoffer Bjørner <chris_bjorner@hotmail.com>
2019-03-04 16:36:52 +0100

    - Kommentarer

--- src/billetautomat/BilletMenu.java
+++ src/billetautomat/BilletMenu.java
@@ -1,3 +1,8 @@
+/*
+    Lavet af: C. Bjørner, U. Esbjørn, M. Repnak, H. Warncke
+
+
+*/
 package billetautomat;
 
 import java.util.Scanner;
@@ -13,24 +18,30 @@ public class BilletMenu
                 
                 while(Boolean.parseBoolean("true"))
                 {
+                    //Valg af funktion
                     System.out.println("[1] Indsæt penge\n[2] Køb billet (" + automat.getBilletpris() + " kr.)\n[3] Tjek saldo\n");
                     System.out.print("Vælg et menupunkt: ");
                     valg = sc.nextInt();
                     System.out.println();
+                    
                     switch(valg)
                     {
+                        //case 1, indsæt pænge
                         case 1:     System.out.print("Indtast hvor meget du vil indsætte: ");
                                     automat.indsætPenge(sc.nextInt());
                                     System.out.println("Din saldo er nu: " + automat.getBalance() + " kr.\n");
                                     break;
                                     
+                        //case 2, køb billet
                         case 2:     automat.udskrivBillet();
                                     System.out.println("Din saldo er nu: " + automat.getBalance() + " kr.\n");
                                     break;
-                                    
+
+                        //case 3, tjek saldo
                         case 3:     System.out.println("Nuværende saldo: " + automat.getBalance() + " kr.\n");
                                     break;
                                     
+                        //default case
                         default:    System.out.println("Indtast venligst kun værdier fra 1 til 10.\n");
                     }
                 }
