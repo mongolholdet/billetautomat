cc5f9877a1bf45c4aefff52a86058ec5929f99f8
Markus Repnak Jacobsen <markusjacobsen@hotmail.com>
2019-03-14 11:07:51 +0100

    Tilføjet menuelementer 5-8

--- src/billetautomat/BilletMenu.java
+++ src/billetautomat/BilletMenu.java
@@ -1,8 +1,7 @@
 /*
     Lavet af: C. Bjørner, U. Esbjørn, M. Repnak, H. Warncke
-
-
 */
+
 package billetautomat;
 
 import java.util.Scanner;
@@ -40,7 +39,27 @@ public class BilletMenu
                         //case 3, tjek saldo
                         case 3:     System.out.println("Nuværende saldo: " + automat.getBalance() + " kr.\n");
                                     break;
+                        
+                        //case 4, tjek billetpris
+                        case 4:     System.out.println("Den nuværende pris er: " + automat.getBilletpris());
+                                    break;
+                        
+                        //case 5, afslut køb (returpenge)  
+                        case 5:     System.out.println("Session afsluttet: " + automat.returpenge() + " nyd din rejse med Borgen Trafikselskab.");
+                                    break;                                    
                                     
+                        //case 6, montør log ind
+                        case 6:     System.out.println("Indtast montørkode: ");
+                                    automat.montørLogInd(sc.nextLine());
+                                    break;
+                        
+                        //case 7, få salgsbeløb       
+                        case 7:     System.out.println("Salgsbeløbet er: " + automat.getSamletSalgsbeløb());
+                                    break;
+                                    
+                        //case 8, udskriv transaktioner  
+                        case 8:     automat.udskrivTransaktioner();
+                                    break;
                         //default case
                         default:    System.out.println("Indtast venligst kun værdier fra 1 til 10.\n");
                     }
