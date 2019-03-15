3fec3263b83550c191c189dd2660eee822cf1fb3
henri <henri@Henrik-Desktop>
2019-03-15 22:12:04 +0100

    Tilføjelse af variabler og metoder til klasse

--- src/billetautomat/IndkoebskurvPost.java
+++ src/billetautomat/IndkoebskurvPost.java
@@ -2,5 +2,28 @@ package billetautomat;
 
 public class IndkoebskurvPost
 {
+    private String navn;
+    private double pris;
+    private int antal;
     
+    public void IndkoebskurvPost(String n, double p)
+    {
+	navn = n;
+	pris = p;
+    }
+    
+    public String getNavn()
+    {
+	return navn;
+    }
+    
+    public double getPris()
+    {
+	return pris;
+    }
+    
+    public int getAntal()
+    {
+        return antal;
+    }
 }
