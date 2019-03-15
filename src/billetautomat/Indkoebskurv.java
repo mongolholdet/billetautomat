e74b5da3f1cb15fc3aece7b09bb6d8c622fc28a0
henri <henri@Henrik-Desktop>
2019-03-15 22:19:26 +0100

    Tilføjelse af ArrayList og metoder til klasse

--- src/billetautomat/Indkoebskurv.java
+++ src/billetautomat/Indkoebskurv.java
@@ -1,6 +1,28 @@
 package billetautomat;
 
+import java.util.ArrayList;
+
 public class Indkoebskurv
 {
+    private ArrayList<Object> poster = new ArrayList<Object>();
     
+    public void Indkoebskurv()
+    {
+        
+    }
+    
+    public void tilfoejPost()
+    {
+        
+    }
+    
+    public void printIndkoebskurv()
+    {
+        
+    }
+    
+    private double getSamletBeloeb()
+    {
+        
+    }
 }
