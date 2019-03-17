f03c32d29aac04c188b05541eb00355916d72de5
Markus <Markus@DESKTOP-NNP52VT>
2019-03-17 11:20:36 +0100

    Tilføjet logLoad for at læse logfilen ind i en arrayliste

--- src/billetautomat/Log.java
+++ src/billetautomat/Log.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class Log
 {
     private String filSti;
+    private ArrayList<LogElement> logData;
     
     public Log(String filStiInit) throws IOException
     {
@@ -24,59 +25,83 @@ public class Log
         logFilLaes.close();
     }
     
-    public void tilfoej(String data) throws IOException
+    public void tilfoej(String dato, String handling, String gamleVaerdi, String nyeVaerdi) throws IOException
     {
         FileWriter logFileWriter = new FileWriter(filSti);
         PrintWriter logFilSkriv = new PrintWriter(logFileWriter);
         
-        logFilSkriv.write(data + "\n"); //Printer til filen
+        logFilSkriv.write(dato + "_" + handling + "_" + gamleVaerdi + "_" + nyeVaerdi + "\n"); //Printer til filen
         logFilSkriv.close();
     }
     
-    public void filtrer(int type) throws IOException
+    private void logLoad() throws IOException
     {
         FileReader logFileReader = new FileReader(filSti);
         BufferedReader logFilLaes = new BufferedReader(logFileReader);
         
-        ArrayList<String> dataArrayList = new ArrayList<String>();
+        logData = new ArrayList<LogElement>();
         String linje = logFilLaes.readLine();
+        
+        String[] linjeArray; 
+        String linjeDato; 
+        String linjeHandling; 
+        String linjeGamleVaerdi;
+        String linjeNyeVaerdi;
+        
         while(linje != null)
         {
-            dataArrayList.add(linje);
+            linjeArray = linje.split("_");
+            linjeDato = linjeArray[0];
+            linjeHandling = linjeArray[1];
+            linjeGamleVaerdi = linjeArray[2];
+            linjeNyeVaerdi = linjeArray[3];
+            logData.add(new LogElement(linjeDato, linjeHandling, linjeGamleVaerdi, linjeNyeVaerdi));
             linje = logFilLaes.readLine();
         }
-        
+    }
+    
+    public void filtrerLog() throws IOException
+    {
+        logLoad();
+        Scanner keyboardInput = new Scanner(System.in);
+        int input;
         while(Boolean.parseBoolean("true")) //lol
         {
             System.out.println("[1] Print hele loggen\n[2] Print loggens køb");
-            switch(type)
+            input = keyboardInput.nextInt();
+            
+            switch(input)
             {
                 
                 //case 0, printer hele loggen
-                case 0:     for(String printLinje : dataArrayList)
-                            {
-                                System.out.println(printLinje);
-                            }
-                //case 1, printer logggen ud fra beløb
-                case 1:     System.out.println("Filtrer efter dyreste beløb[1]\n[2]Billigste beløb\nPrint efter dato");
-                            Scanner keyboardInput = new Scanner(System.in);
-                            int input = keyboardInput.nextInt();
-                            switch(input)
-                            {
-                                case 1:     ArrayList<int> sorteredArrayList = new ArrayList<int>();
-                                            for(String logLinje : dataArrayList)
-                                            {
-                                                if(logLinje.contains("Der blev indsat"))
-                                                {
-                                                    
-                                                    sorteredArrayList.add(Integer.parseInt(logLinje.split(" ")[2]));
-                                                }
-                                                sorteredArrayList.sort();
-                                                
-                                            }
-                                            
-                                            
-                            }
+                case 0:     
+                    for(LogElement logLinje : logData)
+                    {
+                        System.out.println(logLinje.toString());
+                    }
+                    break;
+                    
+                //case 1, printer logggen ud fra beløb eller dato
+                case 1:     
+                    System.out.println("Filtrer efter dyreste beløb[1]\n[2]Billigste beløb\n[3]Print efter dato");
+                    input = keyboardInput.nextInt();
+                    
+                    switch(input)
+                    {
+                        case 1:  
+                            
+                            break;
+                        
+                        case 2:
+                            
+                            break;
+                        
+                        case 3:
+                            
+                            break;
+            
+                    }
+                    break;
             }
         }
     }
