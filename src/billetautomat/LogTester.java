b933494357aa50eb3210dac36b23ae303e307d0b
Markus <Markus@DESKTOP-NNP52VT>
2019-03-18 08:28:13 +0100

    Ryddede op

--- src/billetautomat/LogTester.java
+++ src/billetautomat/LogTester.java
@@ -7,11 +7,8 @@ public class LogTester
 {
     public static void main(String[] args) throws IOException
     {
-        //System.out.println(new File(".").getAbsoluteFile());
-        
         Log logTest = new Log("aktivitetslog.txt");
-        //logTest.tilfoej("1337", "Dabbede paa hatere", "420");
-        logTest.tilfoej(new Date().toString(), "Der blev udskrevet en billet til:", "421","Af test varianten");
+        logTest.tilfoej(new Date().toString(), "Der blev udskrevet en billet til:", "400");
         logTest.filtrerLogMenu();
         
     }
